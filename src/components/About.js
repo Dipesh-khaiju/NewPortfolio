1bb1b441e0eba4c6599dc93db6d3afa43014349c
Dipesh-khaiju <khaijudipesh@gmail.com>
2025-05-20 11:24:53 +0545

    Fixed alt issue in building

--- src/components/About.js
+++ src/components/About.js
@@ -41,6 +41,7 @@ class About extends Component {
                         height: "48px",
                         margin: "7% 5%",
                       }}
+                      alt="express icon"
                     />
                     <Icon
                       icon={pythonIcon}
